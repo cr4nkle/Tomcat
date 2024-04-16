1b35190022606ffdd858eb3b3def6b7529e90e02
Сергей Попович <cr4nkle@yandex.ru>
2024-04-16 16:05:02 +0800

    Добавил класс с константами

---
+++ src/main/java/program/utils/Constant.java
@@ -0,0 +1,10 @@
+package program.utils;
+
+public class Constant {
+    private static final String FIRST_CLIENT_URL = "jdbc:postgresql://localhost:5432/metaInfo";
+    private static final String FIRST_CLIENT_USER = "postgres";
+    private static final String FIRST_CLIENT_PASSWORD = "root";
+    private static final String SECOND_CLIENT_URL = "jdbc:postgresql://localhost:5432/modelStorage";
+    private static final String SECOND_CLIENT_USER = "postgres";
+    private static final String SECOND_CLIENT_PASSWORD = "root";
+}

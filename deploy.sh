70bdbcddf9e1a2bb57bbb611230a54f4679aa0a5
Сергей Попович <cr4nkle@yandex.ru>
2024-05-23 19:49:35 +0800

    Изменил скрипт для развёртывания

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Проверяем наличие необходимых инструментов
-if! command -v mvn &> /dev/null; then
-    echo "Maven could not be found. Installing..."
-    sudo apt-get install maven
-fi
+# Установка Maven
+sudo apt-get update && sudo apt-get install maven
+wait
 
-if! command -v docker &> /dev/null; then
-    echo "Docker could not be found. Installing..."
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
-fi
+# Установка Docker CE, CLI и containerd.io
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+wait
 
-if! command -v docker-compose &> /dev/null; then
-    echo "Docker Compose could not be found. Installing..."
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-fi
+# Загрузка Docker Compose
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+wait
+
+# Установка Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+wait
+
 
 # Настройка Maven для работы с локальным репозиторием
 echo "Configuring Maven local repository..."
@@ -24,7 +26,7 @@ cp settings.xml ~/.m2/settings.xml
 
 # Добавление библиотек в локальный репозиторий
 echo "Adding libraries to local Tomcat..."
-cd /app/Tomcat
+cd /cr4nkle/Tomcat
 for file in../lib/*.jar; do
     groupId=$(echo "$file" | sed -e 's/.*\/\(.*\)\/.*\.jar/\1/')
     artifactId=$(basename "$file".jar)
